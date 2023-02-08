180046b52d26ad5bdc24c564ba364bcc9b5f713c
thecutefinder <123335166+thecutefinder@users.noreply.github.com>
2023-02-07 21:11:14 -0800

    more changes

--- prefspage.js
+++ prefspage.js
@@ -87,30 +87,71 @@ if (mw.config.values.wgPageName === "Genshin_Impact_Wiki:Preferences") {
 
         $("#mw-content-text").append(mobileResponsiveFieldset.$element);
 
-        $("#mw-content-text").append(document.createElement("br"));
+        api
+          .get({
+            action: "query",
+            meta: "userinfo",
+            uiprop: "email",
+            format: "json",
+          })
+          .then(function (prefsData) {
+            var emailPrefsHeader = document.createElement("h2");
+            emailPrefsHeader.innerText = "Email address";
 
-        var saveBtn = new OO.ui.ButtonWidget({
-          label: "Save settings",
-        });
+            emailPrefsHeader.style.border = "0";
+            emailPrefsHeader.style.padding = "0";
+            emailPrefsHeader.style.margin = "0";
+            emailPrefsHeader.style.marginBottom = "5px";
 
-        saveBtn.on("click", async function () {
-          var progressBar = new OO.ui.ProgressBarWidget({
-            progress: false,
+            $("#mw-content-text").append(document.createElement("br"));
+            $("#mw-content-text").append(emailPrefsHeader);
+
+            var emailTextInput = new OO.ui.TextInputWidget({
+              value: prefsData.query.userinfo.email,
+              disabled: true,
+            });
+
+            $("#mw-content-text").append(emailTextInput.$element);
+
+            var changeEmailBtn = new OO.ui.ButtonWidget({
+              label: "Change or remove email address",
+              href: "https://genshinimpact.miraheze.org/wiki/Special:ChangeEmail",
+              target: "_blank",
+            });
+
+            $("#mw-content-text").append(changeEmailBtn.$element);
+
+            $("#mw-content-text").append(document.createElement("br"));
+
+            var mirahezeMetaText = document.createElement("p");
+            mirahezeMetaText.innerHTML =
+              'Most Miraheze-wide settings aren\'t listed here. You can manage them <a target="_blank" rel="nofollow" href="https://meta.miraheze.org/wiki/Special:Preferences">on Miraheze Meta</a>.';
+
+            $("#mw-content-text").append(mirahezeMetaText);
+
+            var saveBtn = new OO.ui.ButtonWidget({
+              label: "Save settings",
+            });
+
+            saveBtn.on("click", async function () {
+              var progressBar = new OO.ui.ProgressBarWidget({
+                progress: false,
+              });
+
+              $("#mw-content-text").append(progressBar.$element);
+
+              await changePrefs(
+                skinDropdown.value,
+                mobileResponsiveCheckbox.selected ? "1" : "0"
+              );
+
+              setTimeout(function () {
+                location.reload();
+              }, 1500);
+            });
+
+            $("#mw-content-text").append(saveBtn.$element);
           });
-
-          $("#mw-content-text").append(progressBar.$element);
-
-          await changePrefs(
-            skinDropdown.value,
-            mobileResponsiveCheckbox.selected ? "1" : "0"
-          );
-
-          setTimeout(function () {
-            location.reload();
-          }, 1500);
-        });
-
-        $("#mw-content-text").append(saveBtn.$element);
       });
     });
   });
