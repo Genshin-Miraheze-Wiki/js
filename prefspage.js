5acadb55d14284e6c0aaa9fdedc9b9d801f483de
thecutefinder <123335166+thecutefinder@users.noreply.github.com>
2023-02-07 16:32:00 -0800

    work in progress changes to preferences page

--- prefspage.js
+++ prefspage.js
@@ -1,10 +1,16 @@
 if (mw.config.values.wgPageName === "Genshin_Impact_Wiki:Preferences") {
+  /*history.replaceState(
+    null,
+    "",
+    "https://genshinimpact.miraheze.org/wiki/Special:Preferences"
+  );*/
+
   mw.loader.using("oojs-ui-core").done(function () {
     $(function () {
-      async function changePrefs(skin) {
+      async function changePrefs(skin, responsive) {
         var params = {
           action: "options",
-          change: "skin=" + skin,
+          change: `skin=${skin}|skin-responsive=${responsive}`,
           format: "json",
         };
 
@@ -58,10 +64,30 @@ if (mw.config.values.wgPageName === "Genshin_Impact_Wiki:Preferences") {
 
         $("#mw-content-text").append(layoutPrefsHeader);
         $("#mw-content-text").append(skinHeader);
-
         $("#mw-content-text").append(skinDropdown.$element);
 
-        //skinDropdown.on("change", skinSelect);
+        $("#mw-content-text").append(document.createElement("br"));
+
+        var mobileResponsiveCheckbox = new OO.ui.CheckboxInputWidget({
+          value: "mobileresponsive",
+          selected: prefsData.query.userinfo.options["skin-responsive"] == "1",
+        });
+
+        mobileResponsiveFieldset = new OO.ui.FieldsetLayout({
+          label: "Enable mobile compatibility",
+        });
+
+        mobileResponsiveFieldset.addItems([
+          new OO.ui.FieldLayout(mobileResponsiveCheckbox, {
+            label:
+              "Automatically adapt the site's layout to screen size on mobile when using a mobile device",
+            align: "inline",
+          }),
+        ]);
+
+        $("#mw-content-text").append(mobileResponsiveFieldset.$element);
+
+        $("#mw-content-text").append(document.createElement("br"));
 
         var saveBtn = new OO.ui.ButtonWidget({
           label: "Save settings",
@@ -74,7 +100,10 @@ if (mw.config.values.wgPageName === "Genshin_Impact_Wiki:Preferences") {
 
           $("#mw-content-text").append(progressBar.$element);
 
-          await changePrefs(skinDropdown.value);
+          await changePrefs(
+            skinDropdown.value,
+            mobileResponsiveCheckbox.selected ? "1" : "0"
+          );
 
           setTimeout(function () {
             location.reload();
