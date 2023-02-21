804eca1cf27f8e9d4144fa0edb8d5225f1f66740
thecutefinder <123335166+thecutefinder@users.noreply.github.com>
2023-02-21 14:46:30 -0800

    work in progress changes

--- socialprofile.js
+++ socialprofile.js
@@ -1,11 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom";
+$.getScript(
+  "https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js"
+).then(
+  function () {
+    $.getScript(
+      "https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js"
+    ).then(
+      function () {
+        const rootNode = document.createElement("div");
+        document.getElementById("mw-content").prepend(rootNode);
+        const root = ReactDOM.createRoot(rootNode);
 
-const rootNode = document.getElementById("#mw-content");
-const root = ReactDOM.createRoot(rootNode);
+        const profileTop = React.createElement("div", { id: "profile-top" });
 
-const profileTop = React.createElement("div", { id: "profile-top" });
+        root.render(profileTop);
 
-root.render(profileTop);
-
-ReactDOM.render(profileTop, root);
+        ReactDOM.render(profileTop, root);
+      },
+      function (e) {
+        // Script failed. X is not available
+        mw.log.error(e.message); // => "Failed to load script"
+      }
+    );
+  },
+  function (e) {
+    // Script failed. X is not available
+    mw.log.error(e.message); // => "Failed to load script"
+  }
+);
